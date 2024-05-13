b71d316fe7ab9f894a28cc6542e78f3f19bfe1ea
zhangbaowen <zhangbaowen@hashdata.cn>
2024-05-13 14:35:21 +0800

    schedule-cbdb-nightly-测试nightly-new-tag测试

---
+++ script/update_tag_number.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 获取最新的 tag
+latest_tag=$(git describe --tags --abbrev=0)
+
+# 从最新的 tag 中提取版本号部分作为变量 version
+version=$(echo $latest_tag | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
+
+# 从 tag 中提取最后一个数字作为 nightly 版本号的基数
+last_number=$(echo $latest_tag | grep -Eo '[0-9]+-nightly' | cut -d'-' -f1)
+
+# 计算下一个 nightly 版本号的最后一位数字
+next_number=$((last_number + 1))
+
+# 设置新的 nightly 版本号
+new_tag="$version.$next_number-nightly"
+
+echo "新的 nightly 版本号为: $new_tag"
